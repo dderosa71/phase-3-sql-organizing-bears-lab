1c7066dac7fc9e18bff312302c1d5fe372ad3d06
dominicderosa <dderosa71@gmail.com>
2021-09-27 17:00:54 -0600

    commit tree 6cac0d83649270eb4bb2f0afb44499c0b3c262c8

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears(
+id INTEGER PRIMARY KEY,
+name INTEGER,
+age TEXT,
+sex INTEGER,
+color TEXT,
+temperament TEXT,
+alive TEXT
+);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Mr. Chocolate", 3, "M", "Brown", "Angry", 0),
+("Rowdy", 3, "M", "Brown", "Angry", 0),
+("Tabitha", 3, "M", "Brown", "Angry", 0),
+("Sergeant Brown", 3, "M", "Brown", "Angry", 0),
+("Melissa", 3, "M", "Brown", "Angry", 0),
+("Grinch", 3, "M", "Brown", "Angry", 0),
+("Wendy", 3, "M", "Brown", "Angry", 0),
+(NULL, 3, "M", "Brown", "Angry", 0);
